82260a8643c3c4b88c6692e1786f4f5f11ad923d
salam <aryangholami222@gamil.com>
2023-11-30 19:26:49 +0330

    edit commit

--- MatProject/MatProject/main.cpp
+++ MatProject/MatProject/main.cpp
@@ -5,10 +5,11 @@
 //  Created by macuser on 11/30/23.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <iostream>
+#include "Mat.h"
+
+int main() {
+    Mat x(Int,2,2);
+    
 }
